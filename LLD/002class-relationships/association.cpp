622b785e6bab639080e016208271946e791ead4d
Gagan Mishra <simonsimple305@gmail.com>
2026-03-19 23:02:54 +0530

    started class relationships

---
+++ LLD/002class-relationships/association.cpp
@@ -0,0 +1,10 @@
+/*
+O1 needs to know about the EXISTENCE of O2 for it to work.
+    - has-a
+    - uses-a
+    - loose-coupling
+
+TYPE of association depends on:
+    - direction (bi-directional, uni-directional)
+    - multiplicity (1-to-1, 1-to-many, many-to-many)
+*/
